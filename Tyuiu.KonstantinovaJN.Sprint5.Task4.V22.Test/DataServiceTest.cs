9767a4fbdff33f895f34b7bc970785afcbc9cdc5
Юлия Константинова <Tyuiu.Konstantinovaajn@outlook.com>
2024-12-21 22:27:30 +0500

    Task4

---
+++ Tyuiu.KonstantinovaJN.Sprint5.Task4.V22.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+using Tyuiu.KonstantinovaJN.Sprint5.Task4.V22.Lib;
+
+namespace Tyuiu.KonstantinovaJN.Sprint5.Task4.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            DataService ds = new DataService();
+
+            string path = Path.Combine(Path.GetTempPath(), "InPutFileTask4V22.txt");
+
+            FileInfo fileinfo = new FileInfo(path);
+
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KonstantinovaJN.Sprint5.Task4.V22/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.KonstantinovaJN.Sprint5.Task4.V22.Lib;
+
+namespace Tyuiu.KonstantinovaJN.Sprint5.Task4.V22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт 5 | Выполнила: Константинова Ю. Н.| ИСПБ-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла.                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнила: Константинова Юлия Николаевна | ИСПБ-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask4V0.txt, в котором есть      *");
+            Console.WriteLine("* вещественное значение. Прочитать значение из файла и подставить вместо Х*");
+            Console.WriteLine("* в формуле y = x^3 * sin(x) - 4x. Вычислить значение по формуле,         *");
+            Console.WriteLine("* полученное значение округлить до трёх знаков после запятой и вернуть    *");
+            Console.WriteLine("* полученный результат на консоль.                                        *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = Path.Combine(Path.GetTempPath(), "InPutFileTask4V22.txt");
+            Console.WriteLine("Данные хранятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
