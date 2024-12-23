ecd78e7c19275504ae833d5b47a097b9c27b3379
Юлия Константинова <Tyuiu.Konstantinovaajn@outlook.com>
2024-12-23 20:38:20 +0500

    Task7

--- Tyuiu.KonstantinovaJN.Sprint5.Task7.V29.Lib/DataService.cs
+++ Tyuiu.KonstantinovaJN.Sprint5.Task7.V29.Lib/DataService.cs
@@ -26,7 +26,7 @@ namespace Tyuiu.KonstantinovaJN.Sprint5.Task7.V29.Lib
                 {
                     for (int i = 0; i < line.Length; i++)
                     {
-                        if (line[i] < -9 || line[i] > 9);
+                        if (line[i] < '0' || line[i] > '9');
                         {
                             strLine += line[i];
                         }
